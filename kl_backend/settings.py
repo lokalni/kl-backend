c6156d0b25c57cdc00a84028e42107697b7bf2c7
Michal Mankowski <maniek.et@gmail.com>
2020-04-02 23:38:25 +0200

    disabling browsable API renderer

--- kl_backend/settings.py
+++ kl_backend/settings.py
@@ -157,12 +157,15 @@ STATICFILES_DIRS = (
 
 REST_FRAMEWORK = {
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend'],
-    'DEFAULT_PERMISSION_CLASSES': [
+    'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
-    ],
-    'DEFAULT_AUTHENTICATION_CLASSES': [
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.SessionAuthentication',
-    ],
+    ),
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    ),
 }
 
 
