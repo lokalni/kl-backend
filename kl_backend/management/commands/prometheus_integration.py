7129638432daf45ccca99c93ed40db1aca0d08d4
Marek Wajdzik <marek@wajdzik.eu>
2020-04-25 12:24:59 +0200

    Added logger

--- kl_backend/management/commands/prometheus_integration.py
+++ kl_backend/management/commands/prometheus_integration.py
@@ -1,6 +1,7 @@
 import requests
 from django.core.management.base import BaseCommand
 from kl_conferences.models import ServerNode
+from logging import getLogger
 
 class Command(BaseCommand):
     help = 'Update server nodes in relation to prometheus alerts'
@@ -9,6 +10,7 @@ class Command(BaseCommand):
         parser.add_argument('prometheus', type=str, help='Prometheus host', required=True)
 
     def handle(self, *args, **options):
+        log = logger.getLogger()
         if ServerNode.objects.exists():
             return
 
@@ -23,4 +25,5 @@ class Command(BaseCommand):
             for alert in alerts:
                 if server.hostname in alert['labels']['instance']:
                     server.enabled = False
+                    log.info("Server {} disabled due to active alerts".format(server.hostname))
             server.save()
